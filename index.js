5ff96fb8020f7c9de0ea9aeaeffd71c9ad4f6bea
Peter Njiru <peternjirum@gmail.com>
2023-03-17 19:36:20 -0400

    completed arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 9;
+const num4 = 5;
 
-// Write your code here
+function multiply() {
+    multiply = num1 * num2;
+    return multiply;
+}
+
+multiply();
+
+function random() {
+    return random = (Math.round(Math.random() * 10));
+}
+
+random();
+
+function mod() {
+    return mod = num3 % num4;
+}
+
+mod();
+
+function max() {
+    return max = Math.max(1, 4, 2, 7, 20);
+}
+
+max();
